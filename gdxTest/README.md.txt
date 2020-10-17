40967c0add79af5c396e4afb48d6a3f2f52f172c
JSanneman <jsanneman@gmail.com>
2020-10-16 20:44:44 -0500

    Finished Map Project assignment

---
+++ gdxTest/README.md.txt
@@ -0,0 +1,9 @@
+BASIC MAP CONTROL PROGRAM
+
+Mouse controls pan with LEFT mouse button.
+Rotate smoothly with LEFT and RIGHT.
+Rotate by 90 degrees holding SHIFT and pressing LEFT or RIGHT.
+Snap back to 0 degrees rotated by holding CTRL and pressing LEFT or RIGHT.
+Zoom in with UP.
+Zoom out with DOWN.
+When zoomed in, press RIGHT mouse button to switch between floors.
--- gdxTest/core/src/gdx/test/GDXTest.java
+++ gdxTest/core/src/gdx/test/GDXTest.java
@@ -15,19 +15,23 @@ import com.badlogic.gdx.math.MathUtils;
 public class GDXTest extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture baseMap;
+	Texture zoomMap1;
+	Texture zoomMap2;
 	OrthographicCamera cam;
 	int WIDTH, HEIGHT;
 	int baseHeight, baseWidth;
 	int mouseX, mouseY; //used to pan with mouse
 	int camAngle = 0; //used to reset camera and help pan correctly
 	boolean keyRelease = true; //used to cleanly rotate 90 degrees
+	int zoomLevel = 0;
 	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		baseMap = new Texture("AerialMap.png");
-		
+		zoomMap1 = new Texture("mapZoomOne.png");
+		zoomMap2 = new Texture("mapZoomTwo.png");
 		WIDTH = Gdx.graphics.getWidth();
 		HEIGHT = Gdx.graphics.getHeight();
 		baseWidth = baseMap.getWidth();
@@ -57,12 +61,23 @@ public class GDXTest extends ApplicationAdapter {
 			if(Gdx.input.isButtonPressed(Buttons.LEFT)) {
 				float deltaX = (mouseX-input.getX());
 				float deltaY = (-mouseY+input.getY());
-				float mouseAngle = (float) ((180/Math.PI)*MathUtils.atan2(-deltaY, -deltaX));
+				float line = (float) Math.sqrt(deltaX*deltaX + deltaY*deltaY) * cam.zoom;
+				float mouseAngle =((180/MathUtils.PI)*MathUtils.atan2(-deltaY, -deltaX));
 				if (mouseAngle < 0) {
 					mouseAngle+=360;
 				}
-				System.out.println("Delta X: " + deltaX + "  Delta Y: " + deltaY + "  Angle: " +mouseAngle + "  Camera Angle: " + camAngle);
-				cam.translate(deltaX*(cam.zoom)*(cos(mouseAngle)-cos(camAngle)),deltaY*(cam.zoom)*(sin(mouseAngle)-sin(camAngle)));
+				//System.out.println("Delta X: " + deltaX + "  Delta Y: " + deltaY + "  Angle: " +mouseAngle + "  Camera Angle: " + camAngle + "  Projection Angle: "+ (mouseAngle-camAngle));
+				cam.translate(-line*cos(mouseAngle-camAngle),-line*sin(mouseAngle-camAngle));
+				//cam.translate(deltaX*(cam.zoom),deltaY*(cam.zoom));
+				updateCam();
+			}
+			
+			if(Gdx.input.isButtonJustPressed(Buttons.RIGHT)) {
+				if (zoomLevel == 0) {
+					zoomLevel++;
+				} else if (zoomLevel == 1) {
+					zoomLevel = 0;
+				}
 				updateCam();
 			}
 			
@@ -81,33 +96,37 @@ public class GDXTest extends ApplicationAdapter {
 		}
 		
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			if (shiftPressed && keyRelease == true) {
-				camAngle-=90;
-				cam.rotate(-90);
-				keyRelease = false;
-			} else if (controlPressed) {
-				cam.rotate(-camAngle);
-				camAngle = 0;
-			} else if (!shiftPressed || keyRelease == true){
-				camAngle -= 1;
-				cam.rotate(-1);
+			if (!Gdx.input.isButtonPressed(Buttons.LEFT)) {
+				if (shiftPressed && keyRelease == true) {
+					camAngle-=90;
+					cam.rotate(-90);
+					keyRelease = false;
+				} else if (controlPressed) {
+					cam.rotate(-camAngle);
+					camAngle = 0;
+				} else if (!shiftPressed || keyRelease == true){
+					camAngle -= 1;
+					cam.rotate(-1);
+				}
+				updateCam();
 			}
-			updateCam();
 		}
 		
 		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			if (shiftPressed && keyRelease == true) {
-				camAngle+=90;
-				cam.rotate(90);
-				keyRelease = false;
-			} else if (controlPressed) {
-				cam.rotate(-camAngle);
-				camAngle = 0;
-			} else if (!shiftPressed || keyRelease == true){
-				camAngle += 1;
-				cam.rotate(1);
+			if (!Gdx.input.isButtonPressed(Buttons.LEFT)) {
+				if (shiftPressed && keyRelease == true) {
+					camAngle+=90;
+					cam.rotate(90);
+					keyRelease = false;
+				} else if (controlPressed) {
+					cam.rotate(-camAngle);
+					camAngle = 0;
+				} else if (!shiftPressed || keyRelease == true){
+					camAngle += 1;
+					cam.rotate(1);
+				}
+				updateCam();
 			}
-			updateCam();
 		}
 		
 		if(!Gdx.input.isKeyPressed(Keys.RIGHT) && !Gdx.input.isKeyPressed(Keys.LEFT)) {
@@ -133,14 +152,23 @@ public class GDXTest extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(8/255f, 135/255f, 39/255f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		handleInput();
 		mouseX = Gdx.input.getX();
 		mouseY = Gdx.input.getY();
 		batch.begin();
 		
-		batch.draw(baseMap, 0, 0);
+		if(cam.zoom >= 0 && cam.zoom < 1) {
+			if (zoomLevel == 0) {
+				batch.draw(zoomMap1, 0, 0);
+			} else {
+				batch.draw(zoomMap2, 0, 0);
+			}
+		} else {
+			batch.draw(baseMap, 0, 0);
+		}
+		
 		batch.end();
 	}
 	
