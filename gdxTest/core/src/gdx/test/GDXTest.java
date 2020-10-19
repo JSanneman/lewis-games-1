bd3d0ba1ee00445c76a02ffad6e4c3a4d4e79051
JSanneman <jsanneman@gmail.com>
2020-10-18 19:52:53 -0500

    Final comments and explanations

--- gdxTest/core/src/gdx/test/GDXTest.java
+++ gdxTest/core/src/gdx/test/GDXTest.java
@@ -29,14 +29,15 @@ public class GDXTest extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		baseMap = new Texture("AerialMap.png");
+		baseMap = new Texture("AerialMap.png"); //My awful MSPaint images
 		zoomMap1 = new Texture("mapZoomOne.png");
 		zoomMap2 = new Texture("mapZoomTwo.png");
 		WIDTH = Gdx.graphics.getWidth();
 		HEIGHT = Gdx.graphics.getHeight();
-		baseWidth = baseMap.getWidth();
+		baseWidth = baseMap.getWidth(); //Could be used for further development
 		baseHeight = baseMap.getHeight();
 		
+		//Used to get the mouse position for use in vectors
 		mouseX = Gdx.input.getX();
 		mouseY = Gdx.input.getY();
 		
@@ -46,6 +47,8 @@ public class GDXTest extends ApplicationAdapter {
 		batch.setProjectionMatrix(cam.combined);
 	}
 	
+	
+	//sin and cos used for code clarity
 	public float sin(float angle) {
 		return MathUtils.sinDeg(angle);
 	}
@@ -61,35 +64,43 @@ public class GDXTest extends ApplicationAdapter {
 			if(Gdx.input.isButtonPressed(Buttons.LEFT)) {
 				float deltaX = (mouseX-input.getX());
 				float deltaY = (-mouseY+input.getY());
-				float line = (float) Math.sqrt(deltaX*deltaX + deltaY*deltaY) * cam.zoom;
-				float mouseAngle =((180/MathUtils.PI)*MathUtils.atan2(-deltaY, -deltaX));
-				if (mouseAngle < 0) {
+				float line = (float) Math.sqrt(deltaX*deltaX + deltaY*deltaY) * cam.zoom; //Takes into account the movement and the scale
+				float mouseAngle =((180/MathUtils.PI)*MathUtils.atan2(-deltaY, -deltaX));//Determines the direction of the mouse movement in degrees
+				if (mouseAngle < 0) { //More for debug than anything
 					mouseAngle+=360;
 				}
-				//System.out.println("Delta X: " + deltaX + "  Delta Y: " + deltaY + "  Angle: " +mouseAngle + "  Camera Angle: " + camAngle + "  Projection Angle: "+ (mouseAngle-camAngle));
+				
+				/**
+				 * The hardest part to understand, the line is composed of both x and y components of the
+				 * initial mouse movement. It is scaled correctly above by multiplying the camera zoom. The
+				 * final angle is the difference between the angle the camera is rotated to, and the angle
+				 * the mouse was moved, creating a sort of projection angle, which is multiplied by the line
+				 * to complete the projection vector. That vector is then split into ITS x and y movements
+				 * which are the final camera translations.
+				 */
 				cam.translate(-line*cos(mouseAngle-camAngle),-line*sin(mouseAngle-camAngle));
-				//cam.translate(deltaX*(cam.zoom),deltaY*(cam.zoom));
 				updateCam();
 			}
 			
 			if(Gdx.input.isButtonJustPressed(Buttons.RIGHT)) {
+				//Determines the zoom level in reference to which floor is visible.
 				if (zoomLevel == 0) {
 					zoomLevel++;
 				} else if (zoomLevel == 1) {
-					zoomLevel = 0;
+					zoomLevel = 0; //Zoom level could be increased but two levels is enough for me
 				}
 				updateCam();
 			}
 			
 		}
 		
-		if(Gdx.input.isKeyPressed(Keys.SHIFT_LEFT)) {
+		if(Gdx.input.isKeyPressed(Keys.SHIFT_LEFT)) { //Helps alter rotation controls
 			shiftPressed = true;
 		} else {
 			shiftPressed = false;
 		}
 		
-		if(Gdx.input.isKeyPressed(Keys.CONTROL_LEFT)) {
+		if(Gdx.input.isKeyPressed(Keys.CONTROL_LEFT)) { //Serves and a reset for rotation controls
 			controlPressed = true;
 		} else {
 			controlPressed = false;
@@ -97,14 +108,15 @@ public class GDXTest extends ApplicationAdapter {
 		
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
 			if (!Gdx.input.isButtonPressed(Buttons.LEFT)) {
-				if (shiftPressed && keyRelease == true) {
+				if (shiftPressed && keyRelease == true) { //Rotated evenly by 90 degrees
+					//camAngle must be kept track of to properly reset the camera and to move while rotated
 					camAngle-=90;
 					cam.rotate(-90);
 					keyRelease = false;
-				} else if (controlPressed) {
+				} else if (controlPressed) { //Resets rotation
 					cam.rotate(-camAngle);
 					camAngle = 0;
-				} else if (!shiftPressed || keyRelease == true){
+				} else if (!shiftPressed || keyRelease == true){ //Rotates smoothly
 					camAngle -= 1;
 					cam.rotate(-1);
 				}
@@ -129,11 +141,13 @@ public class GDXTest extends ApplicationAdapter {
 			}
 		}
 		
+		//The keyRelease is for shift rotating, so it doesn't constantly rotate, effectively turning this into a "isKeyJustPressed" check
 		if(!Gdx.input.isKeyPressed(Keys.RIGHT) && !Gdx.input.isKeyPressed(Keys.LEFT)) {
 			keyRelease = true;
 		}
 		
 		if(Gdx.input.isKeyJustPressed(Keys.DOWN)) {
+			//No in program zoom variable is needed, since cam takes care of that itself
 			cam.zoom += .5;
 			updateCam();
 		}
@@ -144,6 +158,7 @@ public class GDXTest extends ApplicationAdapter {
 		}
 	}
 	
+	//Never not have an updateCam method
 	public void updateCam() {
 		cam.update();
 		batch.setProjectionMatrix(cam.combined);
@@ -159,6 +174,7 @@ public class GDXTest extends ApplicationAdapter {
 		mouseY = Gdx.input.getY();
 		batch.begin();
 		
+		//Tells which image to render where, would be more helpful if they weren't the same size
 		if(cam.zoom >= 0 && cam.zoom < 1) {
 			if (zoomLevel == 0) {
 				batch.draw(zoomMap1, 0, 0);
